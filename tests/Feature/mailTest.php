02ec0318211f4e62cc96ea7ce3a0cfcae261f458
Ofir Katz <ofirkatz94@gmail.com>
2023-11-23 02:08:09 +0100

    test: adjusting test_mail_sent to use mailable

--- tests/Feature/mailTest.php
+++ tests/Feature/mailTest.php
@@ -5,6 +5,7 @@ use Mail;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use App\Mail\MailExample;
 use Tests\TestCase;
 
 class mailTest extends TestCase
@@ -12,11 +13,12 @@ class mailTest extends TestCase
     /**
      * Checks if a mail was sent.
      */
-    public function test_mailsent(): void
+    public function test_mail_sent(): void
     {
         Mail::fake();
+        Mail::send(new MailExample());
 
-        Mail::assertSent(UserInvite::class);
+        Mail::assertSent(MailExample::class);
     }
 
 }
