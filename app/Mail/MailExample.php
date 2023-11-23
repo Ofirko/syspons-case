94578a89c642816fc5a0a7035f1cdb6f261f962f
Ofir Katz <ofirkatz94@gmail.com>
2023-11-23 14:44:54 +0100

    feat: changing mailexample to run 'welcome' view just so it wont break

--- app/Mail/MailExample.php
+++ app/Mail/MailExample.php
@@ -37,7 +37,7 @@ class MailExample extends Mailable
     public function content(): Content
     {
         return new Content(
-            view: 'view.name',
+            view: 'welcome',
         );
     }
 
