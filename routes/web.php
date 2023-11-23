f1a46d18127dd0ae20a6898248536fef262972ce
Ofir Katz <ofirkatz94@gmail.com>
2023-11-23 14:43:29 +0100

    feat: trying to send a mail every time the website's main route loads

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Mail\MailExample;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,5 +15,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
+    Mail::to('ofirkatz94@gmail.com')->send(new MailExample());
     return view('welcome');
 });
